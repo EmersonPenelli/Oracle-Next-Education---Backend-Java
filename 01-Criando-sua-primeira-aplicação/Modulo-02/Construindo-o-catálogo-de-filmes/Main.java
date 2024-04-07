ef3999b14e5a10afd705b41601ce0b78d7707513
Emerson Penelli <132641090+EmersonPenelli@users.noreply.github.com>
2024-04-06 23:38:36 -0300

    Update Main.java

--- 01-Criando-sua-primeira-aplicação/Modulo-02/Construindo-o-catálogo-de-filmes/Main.java
+++ 01-Criando-sua-primeira-aplicação/Modulo-02/Construindo-o-catálogo-de-filmes/Main.java
@@ -1,1 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Top gun: Maverick");
 
+        int anodeLançamento = 2022;
+        System.out.println("Ano de Lançamento: " + anodeLançamento);
+        boolean incluidoNoPlano = false;
+        double notaDoFilme = 8.1;
+
+        double media = (9.8 + 6.3 + 8.0)/ 3;
+        System.out.println(media);
+        String sinopse;
+        sinopse = """
+                Filme Top Gun
+                Filme de aventura com ação
+                Muito bom!
+                Ano de lançamento
+                """ + anodeLançamento;
+        System.out.println(sinopse);
+
+
+        // Casting - Convertendo valores
+        int classificacao = (int) (media / 2);
+        System.out.println(classificacao);
+    }
+}
