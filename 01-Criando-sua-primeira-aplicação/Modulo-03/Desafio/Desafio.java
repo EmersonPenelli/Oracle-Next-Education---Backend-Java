5c3a17edc5d04f11eaa88285b9c47ecc9cc81e93
Emerson Penelli <132641090+EmersonPenelli@users.noreply.github.com>
2024-04-19 16:54:06 -0300

    Initial Commit

---
+++ 01-Criando-sua-primeira-aplicação/Modulo-03/Desafio/Desafio.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Desafio {
+    public static void main(String[] args) {
+        String nome = "Thomas Anderson";
+        String tipoConta = "Corrente";
+        double saldo = 1999.99;
+        int opcao = 0;
+
+        System.out.println("************************************");
+        System.out.println("\nNome do cliente: " + nome);
+        System.out.println("Tipo de conta: " + tipoConta);
+        System.out.println("Saldo atual: " + saldo);
+        System.out.println("\n************************************");
+
+        }
+    }
+}
