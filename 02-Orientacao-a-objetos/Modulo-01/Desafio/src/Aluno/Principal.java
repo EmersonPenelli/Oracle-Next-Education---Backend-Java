fffc1a7d85be96b6e06213302eded005a3e8122f
Emerson Penelli <132641090+EmersonPenelli@users.noreply.github.com>
2024-04-23 17:04:27 -0300

    Delete 02-Orientacao-a-objetos/Modulo-01/Desafio directory

--- 02-Orientacao-a-objetos/Modulo-01/Desafio/src/Aluno/Principal.java
+++
@@ -1,9 +0,0 @@
-public class Principal {
-    public static void main(String[] args) {
-        Aluno meuAluno = new Aluno();
-        meuAluno.nome = "Akemi";
-        meuAluno.idade = 18;
-
-        meuAluno.exibeInformacoes();
-    }
-}
