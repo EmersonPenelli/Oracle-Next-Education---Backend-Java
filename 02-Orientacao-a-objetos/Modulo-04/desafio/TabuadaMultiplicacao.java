83f7a1cabfecbe59eee5a22600389df726a47afc
Emerson Penelli <132641090+EmersonPenelli@users.noreply.github.com>
2024-04-24 16:18:13 -0300

    Initial commit

---
+++ 02-Orientacao-a-objetos/Modulo-04/desafio/TabuadaMultiplicacao.java
@@ -0,0 +1,24 @@
+// Interface Tabuada
+public interface Tabuada {
+    void mostrarTabuada(int numero);
+}
+
+// Classe TabuadaMultiplicacao implementando Tabuada
+public class TabuadaMultiplicacao implements Tabuada {
+    @Override
+    public void mostrarTabuada(int numero) {
+        System.out.println("Tabuada de multiplicação do " + numero + ":");
+        for (int i = 1; i <= 10; i++) {
+            int resultado = numero * i;
+            System.out.println(numero + " x " + i + " = " + resultado);
+        }
+    }
+}
+
+// Classe de Teste
+public class TesteTabuadaMultiplicacao {
+    public static void main(String[] args) {
+        TabuadaMultiplicacao tabuada = new TabuadaMultiplicacao();
+        tabuada.mostrarTabuada(7); // Exemplo com o número 7
+    }
+}
