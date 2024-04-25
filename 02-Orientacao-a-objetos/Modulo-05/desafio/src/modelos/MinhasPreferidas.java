f58a062e09876690149c58e4be65d9eeffdf83a1
Emerson Penelli <132641090+EmersonPenelli@users.noreply.github.com>
2024-04-25 12:45:23 -0300

    Initial commit

---
+++ 02-Orientacao-a-objetos/Modulo-05/desafio/src/modelos/MinhasPreferidas.java
@@ -0,0 +1,13 @@
+package br.edu.minhasmusicas.modelos;
+
+public class MinhasPreferidas {
+
+    public void inclui(Audio audio) {
+        if(audio.getClassificacao() >= 9) {
+            System.out.println(audio.getClassificacao() + " é considerado sucesso absoluto" +
+                    " e preferido por todos!");
+        } else {
+            System.out.println(audio.getClassificacao()  + "também é um dos que todo mundo esta curtindo");
+        }
+    }
+}
