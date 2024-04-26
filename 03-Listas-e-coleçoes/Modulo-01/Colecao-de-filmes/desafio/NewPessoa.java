0ff13014ad24f35c69dba564c4bc76d12117388e
Emerson Penelli <132641090+EmersonPenelli@users.noreply.github.com>
2024-04-26 14:18:51 -0300

    Initial commit

---
+++ 03-Listas-e-coleçoes/Modulo-01/Colecao-de-filmes/desafio/NewPessoa.java
@@ -0,0 +1,7 @@
+Pessoa pessoa1 = new Pessoa("Akemi", 18);
+Pessoa pessoa2 = new Pessoa("Rodrigo", 30);
+Pessoa pessoa3 = new Pessoa("Caroline", 35);
+
+listaDePessoas.add(pessoa1);
+listaDePessoas.add(pessoa2);
+listaDePessoas.add(pessoa3);
