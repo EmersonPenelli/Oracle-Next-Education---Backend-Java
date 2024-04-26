3905e329d6a897d570f279f6f8795e18dc61bb3a
Emerson Penelli <132641090+EmersonPenelli@users.noreply.github.com>
2024-04-26 19:14:05 -0300

    Initial commit

---
+++ 03-Listas-e-coleçoes/Modulo-03/desafio/Animal.java
@@ -0,0 +1,16 @@
+class Animal {
+    // Propriedades e métodos 
+}
+
+class Cachorro extends Animal {
+    // Propriedades e métodos 
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Cachorro cachorro = new Cachorro();
+        Animal animal = (Animal) cachorro;
+
+ 
+    }
+}
