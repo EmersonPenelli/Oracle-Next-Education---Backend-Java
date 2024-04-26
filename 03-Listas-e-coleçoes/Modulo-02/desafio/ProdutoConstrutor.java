496650cda21f1604b8c309fbaa27e74145573b5a
Emerson Penelli <132641090+EmersonPenelli@users.noreply.github.com>
2024-04-26 17:49:42 -0300

    fixed: alterando o código construtor

--- 03-Listas-e-coleçoes/Modulo-02/desafio/ProdutoConstrutor.java
+++ 03-Listas-e-coleçoes/Modulo-02/desafio/ProdutoConstrutor.java
@@ -1,19 +1,16 @@
 public class Produto {
-
     // ... 
 
-    @Override
-    public String toString() {
-        return "Produto [nome=" + nome + ", preco=" + preco + ", quantidade=" + quantidade + "]";
+    public Produto(String nome, double preco, int quantidade) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
     }
 
     public static void main(String[] args) {
-        ArrayList<Produto> listaProdutos = new ArrayList<>();
-        // …
- 
-        for (Produto produto : listaProdutos) {
-            System.out.println(produto);
-        }
+        Produto produtoNovo = new Produto("Novo Produto", 15.99, 3);
+        System.out.println(produtoNovo);
     }
 }
+
 
