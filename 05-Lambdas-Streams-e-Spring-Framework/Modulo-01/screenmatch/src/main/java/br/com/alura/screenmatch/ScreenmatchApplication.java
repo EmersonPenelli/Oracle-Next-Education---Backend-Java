154eed55e9e6b6cae0106b5bb41d1bf0b5087e32
Emerson Penelli <132641090+EmersonPenelli@users.noreply.github.com>
2024-06-20 16:03:40 -0300

    feat: Modelando dados da série

--- 05-Lambdas-Streams-e-Spring-Framework/Modulo-01/screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ 05-Lambdas-Streams-e-Spring-Framework/Modulo-01/screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,6 +1,8 @@
 package br.com.alura.screenmatch;
 
+import br.com.alura.screenmatch.model.DadosSerie;
 import br.com.alura.screenmatch.service.ConsumoApi;
+import br.com.alura.screenmatch.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,7 +17,10 @@ public class ScreenmatchApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		var consumoApi = new ConsumoApi();
-		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=fringe&Season=1&apikey=f1b20bfc");
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=fringe&apikey=f1b20bfc");
 		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
 	}
 }
